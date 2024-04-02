df76c070777349998e90785a9ec4011a84616b9f
RyzhovVictor <ryzhov.dur@gmail.com>
2024-04-02 16:55:10 +0300

    check_problems

--- conventer/main.py
+++ conventer/main.py
@@ -1,9 +1,11 @@
 import os
 import asyncio
-from celery import Celery
+from celery import Celery, result
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException, UploadFile, File
 from fastapi.responses import FileResponse
+from uuid import uuid4
+import pathlib  
 
 import aiofiles
 import subprocess
@@ -13,7 +15,8 @@ app: FastAPI = FastAPI()
 celery: Celery = Celery(
     'tasks',
     broker='redis://localhost:6379/0',
-    backend='redis://localhost:6379/1'
+    backend='redis://localhost:6379/0',
+    broker_connection_retry_on_startup=True
 )
 
 ROOTDIR: str = os.path.dirname(os.path.abspath(__file__))
@@ -22,50 +25,56 @@ TMPDIR: str = os.path.join(ROOTDIR, 'tmp')
 class DirectoryRequest(BaseModel):
     directory: str
 
-def convert_vro_to_mp4(input_file, output_file):
+def convert_vro_to_mp4(input_file):
+    output_file = str(pathlib.Path(input_file).with_suffix('.mp4'))
     command = ['ffmpeg', '-i', input_file, '-c:v', 'copy', '-c:a', 'aac', '-strict', 'experimental', output_file]
     result = subprocess.Popen(command, stdout=subprocess.PIPE)
     output, error = result.communicate()
     if result.returncode != 0:
-        print(f"Ошибка при выполнении команды: {error}")
+        raise Exception('Ошибка конвертации')
     else:
-        print(f"Результат выполнения команды: {output}")
+        return output_file
 
-
-def convert_files_in_dir(directory: str) -> None:
-    for root, _, files in os.walk(directory):
-        for file in files:
-            if file.endswith('.VRO'):
-                vro_file: str = os.path.join(root, file)
-                mp4_file: str = os.path.join(root, f"{file[:-4]}.mp4")
-
-                try:
-                    convert_vro_to_mp4(vro_file, mp4_file)
-                    os.remove(vro_file)
-                except Exception as e:
-                    raise HTTPException(status_code=500, detail=f"Ошибка конвертации файла {vro_file}: {str(e)}")
 
 @celery.task
-def convert_video_task(directory: str) -> None:
-    convert_files_in_dir(directory)
+def convert_video_task(file_path):
+    return convert_vro_to_mp4(file_path)
+
 
 @app.post("/convert-video/")
 async def start_conversion(file: UploadFile = File(...)) -> dict:
     os.makedirs(TMPDIR, exist_ok=True)
-    tmp_file: str = os.path.join(TMPDIR, file.filename)
-
+    name = str(uuid4())
+    tmp_file: str = os.path.join(TMPDIR, name + pathlib.Path(file.filename).suffix)
+    
     async with aiofiles.open(tmp_file, 'wb') as out_file:
         while content := await file.read(1024):
             await out_file.write(content)
 
-    convert_video_task.delay(TMPDIR)
+    task = convert_video_task.delay(tmp_file)
 
-    return {"message": "Процесс конвертации запущен"}
+    return {"message": task.id, "file_name": tmp_file}
+
+
+
+@app.get('/task/status/{task_id}')
+def get_task_status(task_id):
+    task = result.AsyncResult(task_id)
+    return task.status
+
+@app.get('/task/result/{task_id}')
+def get_task_result(task_id):
+    task = result.AsyncResult(task_id)
+    return task.get()
 
 @app.get("/download-video/") 
 async def download_video(file_name: str) -> FileResponse:
     file_path: str = os.path.join(TMPDIR, file_name) 
     if os.path.exists(file_path):
-        return FileResponse(path=file_path, filename=file_name) 
+        response = FileResponse(path=file_path, filename=file_name)
+        os.remove(file_path)
+        
+        return response
     else:
         raise HTTPException(status_code=404, detail="Файл не найден")
+
