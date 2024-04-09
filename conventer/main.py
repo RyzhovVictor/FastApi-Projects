c092903aa2bc28d8723d1d6e1b6fde4de327f7e9
RyzhovVictor <ryzhov.dur@gmail.com>
2024-04-09 12:49:26 +0300

    save_result

--- conventer/main.py
+++ conventer/main.py
@@ -4,7 +4,7 @@ from celery import Celery, result
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException, UploadFile, File
 from fastapi.responses import FileResponse
-from uuid import uuid4
+from uuid import UUID, uuid4
 import pathlib  
 
 import aiofiles
@@ -16,8 +16,10 @@ celery: Celery = Celery(
     'tasks',
     broker='redis://localhost:6379/0',
     backend='redis://localhost:6379/0',
-    broker_connection_retry_on_startup=True
+    broker_connection_retry_on_startup=True,
+    result_backend='redis://localhost:6379/0'
 )
+
 
 ROOTDIR: str = os.path.dirname(os.path.abspath(__file__))
 TMPDIR: str = os.path.join(ROOTDIR, 'tmp')
@@ -26,23 +28,28 @@ class DirectoryRequest(BaseModel):
     directory: str
 
 def convert_vro_to_mp4(input_file):
-    output_file = str(pathlib.Path(input_file).with_suffix('.mp4'))
-    command = ['ffmpeg', '-i', input_file, '-c:v', 'copy', '-c:a', 'aac', '-strict', 'experimental', output_file]
+    output_file = pathlib.Path(input_file).with_suffix('.mp4')
+    command = ['ffmpeg', '-i', input_file, '-c:v', 'copy', '-c:a', 'aac', '-strict', 'experimental', str(output_file)]
     result = subprocess.Popen(command, stdout=subprocess.PIPE)
     output, error = result.communicate()
     if result.returncode != 0:
         raise Exception('Ошибка конвертации')
     else:
-        return output_file
+        return output_file.name
 
 
 @celery.task
 def convert_video_task(file_path):
     return convert_vro_to_mp4(file_path)
 
+@celery.task
+def delete_files_task(file_name):
+    os.remove(os.path.join(TMPDIR, file_name))
+    
+
 
 @app.post("/convert-video/")
-async def start_conversion(file: UploadFile = File(...)) -> dict:
+async def start_conversion(file: UploadFile = File(...)) -> str:
     os.makedirs(TMPDIR, exist_ok=True)
     name = str(uuid4())
     tmp_file: str = os.path.join(TMPDIR, name + pathlib.Path(file.filename).suffix)
@@ -53,28 +60,33 @@ async def start_conversion(file: UploadFile = File(...)) -> dict:
 
     task = convert_video_task.delay(tmp_file)
 
-    return {"message": task.id, "file_name": tmp_file}
+    return str(task.id)
 
 
 
 @app.get('/task/status/{task_id}')
-def get_task_status(task_id):
-    task = result.AsyncResult(task_id)
+async def get_task_status(task_id: UUID):
+    task = result.AsyncResult(str(task_id))
     return task.status
 
 @app.get('/task/result/{task_id}')
-def get_task_result(task_id):
-    task = result.AsyncResult(task_id)
+async def get_task_result(task_id :UUID):
+    task = result.AsyncResult(str(task_id))
     return task.get()
 
 @app.get("/download-video/") 
 async def download_video(file_name: str) -> FileResponse:
     file_path: str = os.path.join(TMPDIR, file_name) 
     if os.path.exists(file_path):
-        response = FileResponse(path=file_path, filename=file_name)
-        os.remove(file_path)
-        
+        response = FileResponse(path=file_path, filename=file_name)  
+        delete_files_task.apply_async(countdown=60, args=[file_name])
         return response
     else:
         raise HTTPException(status_code=404, detail="Файл не найден")
+    
+
+    
+
+
+
 
